31c3fa310ec2175365a2948f0892f7b7521abe22
Andrea Moguel K. <31602026+amoguelk@users.noreply.github.com>
2024-01-04 13:56:40 -0600

    Modified home and settings for Ad Site 1

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,21 +17,23 @@ Including another URLconf
 import os
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.contrib.auth import views as auth_views
 from django.views.static import serve
-from django.views.generic import TemplateView
 
 # Up two folders to serve "site" content
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SITE_ROOT = os.path.join(BASE_DIR, "site")
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="home/main.html"), name="home"),
+    path("", include("home.urls")),
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    re_path(r"^oauth/", include("social_django.urls", namespace="social")),
+    # Apps
     path("polls/", include("polls.urls")),
     path("hello/", include("hello.urls")),
     path("autos/", include("autos.urls")),
     path("cats/", include("cats.urls")),
-    path("accounts/", include("django.contrib.auth.urls")),
     re_path(
         r"^site/(?P<path>.*)$",
         serve,
@@ -39,3 +41,30 @@ urlpatterns = [
         name="site_path",
     ),
 ]
+
+# Serve the favicon - Keep for later
+urlpatterns += [
+    path(
+        "favicon.ico",
+        serve,
+        {
+            "path": "favicon.ico",
+            "document_root": os.path.join(BASE_DIR, "home/static"),
+        },
+    ),
+]
+
+# Switch to social login if it is configured - Keep for later
+try:
+    from . import github_settings
+
+    social_login = "registration/login_social.html"
+    urlpatterns.insert(
+        0,
+        path(
+            "accounts/login/", auth_views.LoginView.as_view(template_name=social_login)
+        ),
+    )
+    print("Using", social_login, "as the login template")
+except:
+    print("Using registration/login.html as the login template")
